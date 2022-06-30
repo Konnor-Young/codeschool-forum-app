f9d1ada988e167bae63f5578ea7b4e2d11cd031a
Konnor-Young <konnor.young93@gmail.com>
2022-06-30 13:08:35 -0600

    delete post not working

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ Vue.component ('this-thread', {
                 </div>`,
 })
 Vue.component ('single-thread', {
-    props: {'thread': Object, 'backtothreads': Function, 'addcomment': Function},
+    props: {'thread': Object, 'backtothreads': Function, 'deletethis': Function},
     template: ` <div>
                     <button @click="backtothreads">Back To Threads</button>
                     <div>
@@ -33,7 +33,7 @@ Vue.component ('single-thread', {
                     <div v-for="post in thread.posts">
                         <h2>{{post.body}}</h2>
                         <p>{{post.user.username}}</p>
-                        <button @click="addcomment">Comment</button>
+                        <button @click=deletethis(thread, post)>Remove</button>
                     </div>
                 </div>`,
     data: function () {
@@ -55,6 +55,7 @@ var app = new Vue({
 
         threadList: [],
         currentThread: {},
+        postMessage: '',
     },
     methods: {
         newUserLogIn: function () {
@@ -79,6 +80,20 @@ var app = new Vue({
         backtoThreads: function () {
             this.showPost = false;
             this.currentThread = {};
+        },
+        addPost: function (thisThread, message) {
+            let id = thisThread._id
+            let newPost = {
+                'thread_id': id,
+                'body': message
+            }
+            this.postPost(newPost, id);
+            this.postMessage = '';
+        },
+        removePost: function (thisThread, thisPost) {
+            let threadID = thisThread._id;
+            let postID = thisPost._id;
+            this.deletePost(threadID, postID);
         },
         getSession: async function () {
             let response = await fetch(`${URL}/session`, {
@@ -150,6 +165,32 @@ var app = new Vue({
             let data = await response.json();
             console.log(response.status);
             this.currentThread = data;
+        },
+        postPost: async function (post, id) {
+            let response = await fetch(`${URL}/post`, {
+                method: 'POST',
+                body: JSON.stringify(post),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+            let data = await response.json();
+            console.log(response.status);
+            console.log(data);
+            if(response.status == 200){
+                this.getThread(id);
+            }
+        },
+        deletePost: async function (thread_id, post_id) {
+            let response = await fetch(`${URL}/thread/${thread_id}/post/${post_id}`, {
+                method: 'DELETE',
+                credentials: 'include'
+            });
+            let data = await response.json();
+            console.log(response.status)
+            console.log(data);
+            this.getThread(thread_id);
         },
     },
     created: function () {
